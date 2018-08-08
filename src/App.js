f8b8b412d81abda29df25b7556667affa0b35fab
rsbabcock <rachael.s.babcock@gmail.com>
2018-08-08 12:55:26 -0500

    added images with alts and title

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import NavBar from './nav/NavBar';
+import GroupTech from './groupTech/groupTech'
 
 class App extends Component {
   render() {
     return (
       <div>
         <NavBar/>
+        <GroupTech/>
       </div>
     );
   }
