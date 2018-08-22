f67ed8e64517454da3f37bd058e295b2f0bd716c
JacobAndrewSmith92 <jacobandrewsmith92@gmail.com>
2018-08-21 21:00:37 -0500

    Added icons that link to gh, li, moreinfo modal.

--- src/student-cards/StudentCard.js
+++ src/student-cards/StudentCard.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import './StudentCard.css'
+import GithubLogo from '../img/cardIcons/Github.png';
+import LinkedInLogo from '../img/cardIcons/Linkedin.png';
+import MoreInfo from '../img/cardIcons/MoreInfo.png';
 
 class StudentCard extends Component {
 
@@ -13,10 +16,12 @@ class StudentCard extends Component {
             <React.Fragment>
                 <div className="card__student">
                     <p>{this.props.student.first_name} {this.props.student.last_name}</p>
-                    <img src={this.props.student.serious_photo} alt={this.props.student.last_name} />
-                    <a target="blank" href={this.props.student.github}>github</a>
-                    <a target="blank" href={this.props.student.linkedin}>linkedIn</a>
-                    <button onClick={this.modalOn}>info</button>
+                    <img className="student__img" src={this.props.student.serious_photo} alt={this.props.student.last_name} />
+                    <div>
+                    <span><a target="blank" href={this.props.student.github}><img className='student__links' src={GithubLogo} alt='Github'/></a></span>
+                    <span onClick={this.modalOn}><img className='student__info' src={MoreInfo} alt='More Info'/></span>
+                    <span><a target="blank" href={this.props.student.linkedin}><img className='student__links' src={LinkedInLogo} alt='LinkedIn'/></a></span>
+                    </div>
                 </div>
             </React.Fragment>
         )
