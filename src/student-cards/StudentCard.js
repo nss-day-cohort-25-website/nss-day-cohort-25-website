db7710d49757f611e79f41edf36e84e3eb633418
JacobAndrewSmith92 <jacobandrewsmith92@gmail.com>
2018-08-23 00:19:24 -0500

    Continued styling. Zzz 12:20am zzZ

--- src/student-cards/StudentCard.js
+++ src/student-cards/StudentCard.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react'
 import './StudentCard.css'
-import GithubLogo from '../img/cardIcons/Github.png';
-import LinkedInLogo from '../img/cardIcons/Linkedin.png';
-import MoreInfo from '../img/cardIcons/MoreInfo.png';
 
 class StudentCard extends Component {
 
@@ -14,15 +11,15 @@ class StudentCard extends Component {
     render() {
         return (
             <React.Fragment>
-                <div className="card__student">
-                    <p>{this.props.student.first_name} {this.props.student.last_name}</p>
-                    <img className="student__img" src={this.props.student.serious_photo} alt={this.props.student.last_name} />
-                    <div>
-                    <span><a target="blank" href={this.props.student.github}><img className='student__links' src={GithubLogo} alt='Github'/></a></span>
-                    <span><a target="blank" href={this.props.student.linkedin}><img className='student__links' src={LinkedInLogo} alt='LinkedIn'/></a></span>
-                    <span onClick={this.modalOn}><img className='student__info' src={MoreInfo} alt='More Info'/></span>
+                    <div className="card__student">
+                        <p>{this.props.student.first_name} {this.props.student.last_name}</p>
+                        <img className="student__img" src={this.props.student.serious_photo} alt={this.props.student.last_name} />
+                        <div>
+                        <span><a target="blank" href={this.props.student.github}><i class="fab fa-github student__links"></i></a></span>
+                        <span><a target="blank" href={this.props.student.linkedin}><i class="fab fa-linkedin student__links"></i></a></span>
+                        <span onClick={this.modalOn}><i class="fas fa-info-circle student__links"></i></span>
+                        </div>
                     </div>
-                </div>
             </React.Fragment>
         )
     }
